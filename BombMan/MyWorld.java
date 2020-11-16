5a00a65f4f92871c3e79fe5f7b2b8380809dbb51
DarelA-Program <73359519+DarelA-Program@users.noreply.github.com>
2020-11-16 18:02:37 -0500

    Updated the Greenfoot world

--- BombMan/MyWorld.java
+++ BombMan/MyWorld.java
@@ -1,4 +1,3 @@
-// WARNING: This file is auto-generated and any changes to it will be overwritten
 import lang.stride.*;
 import java.util.*;
 import greenfoot.*;
@@ -15,5 +14,14 @@ public class MyWorld extends World
     public MyWorld()
     {
         super(600, 400, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }

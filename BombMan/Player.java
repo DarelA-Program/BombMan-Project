a325563f1034791607136e9e3795c2d7dd610fd1
DarelA-Program <73359519+DarelA-Program@users.noreply.github.com>
2020-11-17 16:48:27 -0500

    Added The Project

---
+++ BombMan/Player.java
@@ -0,0 +1,61 @@
+import lang.stride.*;
+import greenfoot.*;
+
+/**
+ * Write a description of class Player here.
+ * @author (your name) @version (a version number or a date)
+ */
+public class Player extends Actor
+{
+    /**
+     * this method is called when the Player touches the coin the coin will disapear
+     */
+   public void act() {
+        checkKeyPressed();
+        if (isTouching(Coin.class)) {
+          removeTouching(Coin.class);
+        }
+   }
+  
+    
+
+    /**
+     * This method is made for the character to move left, right, up, down;
+     */
+    
+    public void checkKeyPressed(){
+    if(Greenfoot.isKeyDown("up")) {
+       setLocation(getX(), getY() - 5);
+        
+    }
+    if(Greenfoot.isKeyDown("down")) {
+       setLocation(getX(), getY() + 5); 
+        
+    }
+    if(Greenfoot.isKeyDown("left")) {
+       setLocation(getX() - 5, getY()); 
+        
+    }
+    if(Greenfoot.isKeyDown("right")) {
+       setLocation(getX() + 5, getY()); 
+        
+    }
+}
+}
+
+    
+     
+
+
+
+    
+        
+        
+       
+      
+
+
+
+
+
+
